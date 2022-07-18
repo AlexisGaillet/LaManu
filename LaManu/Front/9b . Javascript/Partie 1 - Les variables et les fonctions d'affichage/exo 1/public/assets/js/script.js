4289c5c082fbf4d6f5439ff5bc3ef6e71ec09153
AlexisGaillet <alexisgaillet36@gmail.com>
2022-07-18 16:53:48 +0200

    18/07/2022 js

---
+++ LaManu/Front/9b . Javascript/Partie 1 - Les variables et les fonctions d'affichage/exo 1/public/assets/js/script.js
@@ -0,0 +1,182 @@
+                // PARTIE 1 - LES VARIABLES ET LES FONCTIONS D'AFFICHAGE
+
+        // EXERCICE 1
+
+// let message = 'message en alert';
+// alert(message);
+
+
+
+
+        // EXERCICE 2
+
+// let firstPart='Première partie et la';
+// let lastPart='seconde partie du message';
+
+// alert(firstPart + lastPart);
+
+
+
+
+        // EXERCICE 3
+
+// let firstName='Alexis';
+// let age=21;
+// let city='Amiens';
+
+// alert(`Bonjour, je m\'appelle ${firstName}, j\'ai ${age} ans et j\'habite ${city} `);
+
+
+
+
+        // EXERCICE 4
+
+// let firstName=prompt("Le prénom ?");
+// let age=prompt("Ton age ?");
+// let city=prompt("La ville ?");
+
+// alert(`Bonjour, je m\'appelle ${firstName}, j\'ai ${age} ans et j\'habite à ${city} `);
+
+
+
+
+        // EXERCICE 5
+
+// let firstNumber=prompt("Premier nombre ?");
+// let secondNumber=prompt("Second nombre ?");
+// let calcResult= Number(firstNumber)+Number(secondNumber);
+
+// alert(`Le résultat est ${calcResult} car ${firstNumber} + ${secondNumber} = ${calcResult}`);
+
+
+
+
+
+
+                // PARTIE 2 - LES CONDITIONS
+
+        // EXERCICE 1
+
+// let number= 22;
+
+// if (number > 21) {
+//     alert('message')
+// };
+
+
+
+
+        // EXERCICE 2
+
+// let age=prompt("Ton age ?");
+
+// if (age > 18) {
+//     alert('Vous êtes majeur');
+// } else {
+//     alert('Vous êtes mineur');
+// }
+
+
+
+
+        // EXERCICE 3
+
+// let number=prompt("Un nombre ?")
+
+// if (number%2 == 0) {
+//     alert('Nompre Pair');
+// } else {
+//     alert('Nombre impair');
+// }
+
+
+
+
+        // EXERCICE 4
+
+// let note=prompt("Une note ?")
+
+// if (note > 0 && note < 4) {
+//     alert('Nul');
+// } else if (note >= 4 && note < 6) {
+//     alert('Moyen');
+// } else if (note >= 6 && note < 8) {
+//     alert('Assez bien');
+// } else if (note >= 8 && note < 9) {
+//     alert('Bien');
+// } else if (note >= 9 && note <= 10) {
+//     alert('Excellent');
+// }
+
+
+
+
+        // EXERCICE 5
+
+// let day=2;
+
+// switch (day) {
+//     case 1:
+//         alert('Lundi');
+//         break;
+//     case 2:
+//         alert('Mardi');
+//         break;
+//     case 3:
+//         alert('Mercredi');
+//         break;
+//     case 4:
+//         alert('Jeudi');
+//         break;
+//     case 5:
+//         alert('Vendredi');
+//         break;
+//     case 6:
+//         alert('Samedi');
+//         break;
+//     case 7:
+//         alert('Dimanche');
+//         break;
+// }
+
+
+
+
+
+
+                // PARTIE 3 - LES BOUCLES
+
+        // EXERCICE 1
+
+for (let years= 2020; years <= 2030; years++) {
+        if (years % 4 === 0 && years % 100 !== 0 || years %400 === 0) {
+        document.write('<div class="red">' + years + '</div>')
+        } else {
+                document.write('<div class="black">' + years + '</div>')
+        }
+}
+
+
+
+
+        // EXERCICE 2
+
+
+
+
+        // EXERCICE 3
+
+
+
+
+        // EXERCICE 4
+
+
+
+
+        // EXERCICE 5
+
+
+
+
+        // EXERCICE 6
